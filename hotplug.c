cc58710d5e21cf60f7360194d6988a7d16a18b3e
jbleyel <jbleyel@gmx.net>
2024-10-20 12:38:23 +0200

    add UUID for remove

--- hotplug.c
+++ hotplug.c
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
 					}
 					else if(strcmp(action, "remove") == 0)
 					{
-						snprintf(data, sizeof(data) - 1, "ACTION=%s\nDEVPATH=%s\nID_TYPE=%s\nDEVTYPE=%s\nDEVNAME=%s", action, devpath, getenv("ID_TYPE"), getenv("DEVTYPE"), getenv("DEVNAME"));
+						snprintf(data, sizeof(data) - 1, "ACTION=%s\nDEVPATH=%s\nID_TYPE=%s\nDEVTYPE=%s\nDEVNAME=%s\nID_FS_UUID=%s", action, devpath, getenv("ID_TYPE"), getenv("DEVTYPE"), getenv("DEVNAME"), getenv("ID_FS_UUID"));
 						data[sizeof(data) - 1] = 0;
 						printf("%s\n", data);
 						send(sd, data, strlen(data) + 1, 0);
